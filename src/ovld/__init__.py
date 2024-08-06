554c49933011919fb359b5c8c82f878117302c8b
Olivier Breuleux <breuleux@gmail.com>
2024-08-06 14:30:21 -0400

    Use __all__ in __init__

--- src/ovld/__init__.py
+++ src/ovld/__init__.py
@@ -1,3 +1,48 @@
-from .core import *
-from .utils import *
+from .core import (
+    MultiTypeMap,
+    Ovld,
+    OvldBase,
+    OvldCall,
+    OvldMC,
+    TypeMap,
+    extend_super,
+    is_ovld,
+    ovld,
+    ovld_dispatch,
+)
+from .utils import (
+    BOOTSTRAP,
+    MISSING,
+    Dataclass,
+    Named,
+    deferred,
+    exactly,
+    has_attribute,
+    keyword_decorator,
+    meta,
+    strict_subclass,
+)
 from .version import version as __version__
+
+__all__ = [
+    "MultiTypeMap",
+    "Ovld",
+    "OvldBase",
+    "OvldCall",
+    "OvldMC",
+    "TypeMap",
+    "extend_super",
+    "is_ovld",
+    "ovld",
+    "ovld_dispatch",
+    "BOOTSTRAP",
+    "MISSING",
+    "Dataclass",
+    "Named",
+    "deferred",
+    "exactly",
+    "has_attribute",
+    "meta",
+    "keyword_decorator",
+    "strict_subclass",
+]
