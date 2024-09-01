e87e76fd57ed2e607d0c16dd20bbb0fe711b24ca
Olivier Breuleux <breuleux@gmail.com>
2024-09-01 16:31:53 -0400

    Generate custom dispatch functions

--- src/ovld/recode.py
+++ src/ovld/recode.py
@@ -1,8 +1,11 @@
 import ast
 import inspect
+import runpy
+import tempfile
 import textwrap
+from collections import defaultdict
 from itertools import count
-from types import CodeType, FunctionType
+from types import CodeType, FunctionType, GenericAlias
 
 from .utils import Unusable, UsageError
 
@@ -12,6 +15,197 @@ recurse = Unusable(
 call_next = Unusable(
     "call_next() can only be used from inside an @ovld-registered function."
 )
+
+
+_tempfiles = []
+
+
+dispatch_template = """
+class MISSING:
+    pass
+
+def __DISPATCH__(self, {args}):
+    {inits}
+    {body}
+
+    method = self.map[({lookup})]
+    return method({slf}{posargs})
+"""
+
+
+class ArgumentAnalyzer:
+    def __init__(self):
+        self.name_to_positions = defaultdict(set)
+        self.position_to_names = defaultdict(set)
+        self.counts = defaultdict(lambda: [0, 0])
+        self.complex_transforms = set()
+        self.total = 0
+        self.is_method = None
+
+    def add(self, fn):
+        sig = inspect.signature(fn)
+        is_method = False
+        for i, (name, param) in enumerate(sig.parameters.items()):
+            if name == "self":
+                assert i == 0
+                is_method = True
+                continue
+            ann = param.annotation
+            if isinstance(ann, str):
+                ann = eval(ann, getattr(fn, "__globals__", {}))
+            is_complex = isinstance(ann, GenericAlias)
+            if param.kind is inspect._POSITIONAL_ONLY:
+                cnt = self.counts[i]
+                self.position_to_names[i].add(None)
+                key = i
+            elif param.kind is inspect._POSITIONAL_OR_KEYWORD:
+                cnt = self.counts[i]
+                self.position_to_names[i].add(param.name)
+                self.name_to_positions[param.name].add(i)
+                key = i
+            elif param.kind is inspect._KEYWORD_ONLY:
+                cnt = self.counts[param.name]
+                self.name_to_positions[param.name].add(param.name)
+                key = param.name
+            elif param.kind is inspect._VAR_POSITIONAL:
+                raise TypeError("ovld does not support *args")
+            elif param.kind is inspect._VAR_KEYWORD:
+                raise TypeError("ovld does not support **kwargs")
+
+            if is_complex:
+                self.complex_transforms.add(key)
+
+            cnt[0] += 1 if param.default is inspect._empty else 0
+            cnt[1] += 1
+
+        self.total += 1
+
+        if self.is_method is None:
+            self.is_method = is_method
+        elif self.is_method != is_method:
+            raise TypeError(
+                "Some, but not all registered methods define `self`. It should be all or none."
+            )
+
+    def compile(self):
+        if any(
+            len(pos) != 1
+            for _name, pos in self.name_to_positions.items()
+            if (name := _name) is not None
+        ):
+            raise TypeError(
+                f"Argument {name} is found both in a positional and keyword setting."
+            )
+        npositional = 0
+        positional = []
+        for pos, names in sorted(self.position_to_names.items()):
+            required, total = self.counts[pos]
+            name = f"_ovld_arg{pos}"
+            if len(names) == 1 and total == self.total:
+                name = list(names)[0]
+            else:
+                npositional = pos + 1
+            positional.append((name, required == self.total))
+
+        keywords = []
+        for key, (name,) in self.name_to_positions.items():
+            if isinstance(name, int):
+                pass  # ignore positional arguments
+            else:
+                assert key == name
+                required, total = self.counts[key]
+                keywords.append((name, required == self.total))
+
+        return positional[:npositional], positional[npositional:], keywords
+
+    def generate_dispatch(self):
+        po, pn, kw = self.compile()
+
+        inits = set()
+
+        argsstar = ""
+        kwargsstar = ""
+        targsstar = ""
+
+        args = []
+        body = [""]
+        posargs = []
+        lookup = []
+
+        def _positional(key, k, necessary):
+            nonlocal argsstar, targsstar
+            lookup_fn = (
+                "self.map.transform"
+                if key in self.complex_transforms
+                else "type"
+            )
+            if necessary:
+                args.append(k)
+                posargs.append(k)
+                lookup.append(f"{lookup_fn}({k})")
+            else:
+                args.append(f"{k}=MISSING")
+                argsstar = "*ARGS"
+                targsstar = "*TARGS"
+                inits.add("ARGS = []")
+                inits.add("TARGS = []")
+                body.append(f"if {k} is not MISSING:")
+                body.append(f"    ARGS.append({k})")
+                body.append(f"    TARGS.append({lookup_fn}({k}))")
+
+        i = 0
+        for k, necessary in po:
+            _positional(i, k, necessary)
+            i += 1
+
+        args.append("/")
+
+        for k, necessary in pn:
+            _positional(i, k, necessary)
+            i += 1
+
+        if kw:
+            args.append("*")
+
+        for k, necessary in kw:
+            lookup_fn = (
+                "self.map.transform" if k in self.complex_transforms else "type"
+            )
+            if necessary:
+                args.append(f"{k}")
+                posargs.append(f"{k}={k}")
+                lookup.append(f"({k!r}, {lookup_fn}({k}))")
+            else:
+                args.append(f"{k}=MISSING")
+                kwargsstar = "**KWARGS"
+                targsstar = "*TARGS"
+                inits.add("KWARGS = {}")
+                inits.add("TARGS = []")
+                body.append(f"if {k} is not MISSING:")
+                body.append(f"    KWARGS[{k!r}] = {k}")
+                body.append(f"    TARGS.append(({k!r}, {lookup_fn}({k})))")
+
+        posargs.append(argsstar)
+        posargs.append(kwargsstar)
+        lookup.append(targsstar)
+
+        code = dispatch_template.format(
+            inits="\n    ".join(inits),
+            args=", ".join(x for x in args if x),
+            posargs=", ".join(x for x in posargs if x),
+            body="\n    ".join(x for x in body if x),
+            lookup=", ".join(x for x in lookup if x) + ",",
+            slf="self.obj, " if self.is_method else "",
+        )
+        return self.instantiate_code("__DISPATCH__", code)
+
+    def instantiate_code(self, symbol, code):
+        tf = tempfile.NamedTemporaryFile("w")
+        _tempfiles.append(tf)
+        tf.write(code)
+        tf.flush()
+        glb = runpy.run_path(tf.name)
+        return glb[symbol]
 
 
 _current = count()
@@ -90,6 +284,7 @@ def rename_function(fn, newname):
     new_fn = FunctionType(
         newcode, fn.__globals__, newname, fn.__defaults__, fn.__closure__
     )
+    new_fn.__kwdefaults__ = fn.__kwdefaults__
     new_fn.__annotations__ = fn.__annotations__
     return new_fn
 
@@ -236,6 +431,7 @@ def recode(fn, ovld, recurse_sym, call_next_sym, newname):
     new_fn = FunctionType(
         new_code, fn.__globals__, newname, fn.__defaults__, fn.__closure__
     )
+    new_fn.__kwdefaults__ = fn.__kwdefaults__
     new_fn.__annotations__ = fn.__annotations__
     new_fn = rename_function(new_fn, newname)
     new_fn.__globals__[ovld_mangled] = ovld
